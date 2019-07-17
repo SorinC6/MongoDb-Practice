33ade06684bd0dbf23143692a692375c750b257c
Sorin Chis <kish.sorin@yahoo.com>
2019-07-17 16:48:50 +0300

    added mocha for testing

---
+++ test/demo_test.js
@@ -0,0 +1,10 @@
+//const mocha = require("mocha");
+const assert = require("assert");
+
+//Describe tests
+describe("some demo tests", () => {
+  //Create tests
+  it("adds two numbers", () => {
+    assert(2 + 3 === 5);
+  });
+});
