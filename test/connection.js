adaa4c6dd3b99b67b933e31b4b18807ccc149423
Sorin Chis <kish.sorin@yahoo.com>
2019-07-17 15:57:03 +0300

    added mongoDb locally and create the connection

---
+++ test/connection.js
@@ -0,0 +1,12 @@
+const mongoose = require("mongoose");
+
+//connect to mongoDb
+mongoose.connect("mongodb://localhost/testare");
+
+mongoose.connection
+  .once("open", () => {
+    console.log("Connection Working yeee! ready to rock ");
+  })
+  .on("error", error => {
+    console.log("Connection Error ", error);
+  });
